ae4818d02abc79da83c560ee2b171260f9826923
guutut <kuba1filpachilp592@gmail.com>
2026-02-22 12:48:40 +0100

    Implement Windows resource compilation in build.rs
    Add Windows resource compilation for icon support

---
+++ build.rs
@@ -0,0 +1,10 @@
+extern crate winres;
+
+fn main() {
+  if cfg!(target_os = "windows") {
+    let mut res = winres::WindowsResource::new();
+    res.set_icon("src/epik.ico"); // yes, we have an icon
+    res.compile().unwrap();
+  }
+}
+// credits to 4gla (lvktr)!
